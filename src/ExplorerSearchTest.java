1c944ccfc772a5d32c36c9db2f32f825599ebc2a
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-06-02 01:46:46 -0700

    Created testStart_originalStart test method

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -5,11 +5,11 @@ public class ExplorerSearchTest {
     @Test
     public void testReachableArea_someUnreachable() {
         int[][] island = {
-            {1,1,1,3,1,1},
-            {3,2,3,1,3,1},
-            {1,1,1,1,3,3},
-            {3,1,2,1,0,1},
-            {1,1,1,2,1,1},
+                { 1, 1, 1, 3, 1, 1 },
+                { 3, 2, 3, 1, 3, 1 },
+                { 1, 1, 1, 1, 3, 3 },
+                { 3, 1, 2, 1, 0, 1 },
+                { 1, 1, 1, 2, 1, 1 },
         };
         int actual = ExplorerSearch.reachableArea(island);
         assertEquals(14, actual);
@@ -17,4 +17,17 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+    @Test
+    public void testStart_originalStart() {
+        int[][] island = {
+                { 1, 1, 1, 3, 1, 1 },
+                { 3, 2, 3, 1, 3, 1 },
+                { 1, 1, 1, 1, 3, 3 },
+                { 3, 1, 2, 1, 0, 1 },
+                { 1, 1, 1, 2, 1, 1 },
+        };
+        int[] expected = { 3, 4 };
+
+        assertArrayEquals(expected, ExplorerSearch.start(island));
+    }
 }
